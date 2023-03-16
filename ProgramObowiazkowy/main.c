6ed81d4c9b0101026a28e3ed0245234b942bad41
HalmonCelman <halmoncelman@gmail.com>
2023-03-16 20:55:17 +0100

    Druga Czesc - Konwersje

--- ProgramObowiazkowy/main.c
+++ ProgramObowiazkowy/main.c
@@ -1,6 +1,8 @@
 #undef NULL
 #define NULL (0)
 
+// ***************** czesc 1 ************************
+
 typedef enum{
 DIFFRENT,
 EQUAL
@@ -39,14 +41,58 @@ void ReplaceCharactersInString(char pcString[],char cOldChar,char cNewChar){
     }
   }
 }
+//**********************************************************
+
+//********************* czesc 2 *****************************
+
+typedef enum{
+OK,
+ERROR
+} Result;
+
+void UIntToHex(unsigned int uiValue, char pcStr[]){
+	pcStr[0]='0';
+	pcStr[1]='x';
+	unsigned char ucNibbleValue;
+	for(unsigned char ucNibbleCounter=0;ucNibbleCounter<8;ucNibbleCounter++){
+		ucNibbleValue=((uiValue>>(ucNibbleCounter*4)) & 0xF);
+		pcStr[9-ucNibbleCounter]=((char)(ucNibbleValue+((ucNibbleValue>9)?('A'-10):('0'))));
+	}
+	pcStr[10]=NULL;
+}
+
+Result eHexStringToUInt(char pcStr[],unsigned int *puiValue){
+	if (!(('0' == pcStr[0]) && ('x' == pcStr[1]))){
+		return ERROR;
+	}
+	*(puiValue)=0;
+	char cCharacterValue;
+	for(unsigned char ucCharacterCounter=0;pcStr[ucCharacterCounter]!=NULL;ucCharacterCounter++){
+		if(4==ucCharacterCounter){
+			return ERROR;
+		}
+		cCharacterValue=pcStr[ucCharacterCounter+2];
+		*(puiValue)=(*(puiValue)<<4)+cCharacterValue-((cCharacterValue>='A')?('A'-10):('0'));
+	}
+	return OK;
+}
+
+void AppendUIntToString (unsigned int uiValue, char pcDestinationStr[]){
+	unsigned char ucNullPosition;
+	for(ucNullPosition=0;pcDestinationStr[ucNullPosition]!=NULL;ucNullPosition++);
+	UIntToHex(uiValue,&pcDestinationStr[ucNullPosition]);
+}
+
+
+//****************************************************************
 
 
 int main(){
-char cStr1[254];
 
-CopyString("sttring2\0",cStr1);
+	char cStr1[254];
+	CopyString("costam\0",cStr1);
 
-ReplaceCharactersInString(cStr1,'t','A');
-
-return 0;
+	AppendUIntToString(0xFAB4,cStr1);
+	
+	return 0;
 }
