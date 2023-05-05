a6ed8cca3e862e79f8c6b6675ee0e9dc76e66a2e
HalmonCelman <halmoncelman@gmail.com>
2023-05-05 19:17:17 +0200

    Timery

--- Laby/Current/7_TIMERY/main.c
+++ Laby/Current/7_TIMERY/main.c
@@ -4,14 +4,6 @@
 #include "keyboard.h"
 #include "timer.h"
 
-void Delay(unsigned int uiDelayTime);
-
-void Delay(unsigned int uiDelayTime){
-	WaitOnTimer0(uiDelayTime*1000);
-}
-
-
-
 
 //main
 int main(void){
@@ -19,34 +11,10 @@ int main(void){
 	LedInit();
 	KeyboardInit();
 
-	typedef enum {MOVE_LEFT, MOVE_RIGHT, STAY} LedState;
-  LedState eLedState = STAY;
-
   while(1){
-    switch(eLedState){
-      case MOVE_LEFT:
-        LedStepLeft();
-				if(BUTTON_1 == eKeyboardRead()){
-          eLedState = STAY;
-				}
-        break;
-      case STAY:
-        if(BUTTON_2 == eKeyboardRead()){
-				  eLedState=MOVE_RIGHT;
-        }
-				else if(BUTTON_0 == eKeyboardRead()){
-          eLedState = MOVE_LEFT;
-				}
-        break;
-			case MOVE_RIGHT:
-        LedStepRight();
-				if(BUTTON_1 == eKeyboardRead()){
-          eLedState = STAY;
-				}
-        break;	
-			
-    }
-		Delay(100);
+    LedStepLeft();
+		InitTimer0Match0(100000);
+		WaitOnTimer0Match0();
   }
 	return 0;
 }
--- Laby/Current/7_TIMERY/timer.h
+++ Laby/Current/7_TIMERY/timer.h
@@ -4,4 +4,8 @@
 void InitTimer0(void);
 void WaitOnTimer0(unsigned int uiTime);
 
+void InitTimer0Match0(unsigned int iDelayTime);
+void WaitOnTimer0Match0(void);
+
+
 #endif
