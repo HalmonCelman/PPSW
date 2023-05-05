240efb339a7c2dc06b98387dc98f3b01e0d989dd
HalmonCelman <halmoncelman@gmail.com>
2023-05-05 21:48:20 +0200

    SERWO 4

--- Laby/Current/9_SERWO/main.c
+++ Laby/Current/9_SERWO/main.c
@@ -4,81 +4,8 @@
 #include "led.h"
 #include "keyboard.h"
 #include "timer_interrupts.h"
+#include "servo.h"
 
-#define DETECTOR_bm (1<<10)
-
-typedef enum {ACTIVE, INACTIVE} DetectorState;
-
-enum ServoState {CALLIB, IDLE, IN_PROGRESS};
-
-struct Servo
-{
-enum ServoState eState; unsigned int
-uiCurrentPosition; unsigned int
-uiDesiredPosition;
-}; struct Servo sServo;
-
-
-void Automat(void);
-void DetectorInit(void);
-DetectorState eReadDetector(void);
-
-void ServoCallib(void){
-  sServo.eState=CALLIB;
-}
-
-void ServoInit(unsigned int uiServoFrequency){
-	ServoCallib();
-	LedInit();
-	Timer0Interrupts_Init(1000000/uiServoFrequency,&Automat);
-}
-
-void ServoGoTo(unsigned int uiPosition){
-	sServo.uiDesiredPosition=uiPosition;
-}
-
-void Automat(void){
-		
-	switch(sServo.eState){
-		  case CALLIB:
-			  if(ACTIVE == eReadDetector()){
-				  sServo.eState = IDLE;
-					sServo.uiCurrentPosition=0;
-					sServo.uiDesiredPosition=0;
-        }else{
-					LedStepLeft();
-				}
-			  break;
-      case IDLE:
-				if(sServo.uiCurrentPosition != sServo.uiDesiredPosition){
-          sServo.eState = IN_PROGRESS;
-				}
-        break;
-      case IN_PROGRESS:
-        if(sServo.uiCurrentPosition < sServo.uiDesiredPosition){
-          LedStepRight();
-					sServo.uiCurrentPosition++;
-				} else if(sServo.uiCurrentPosition > sServo.uiDesiredPosition){
-          LedStepLeft();
-					sServo.uiCurrentPosition--;
-				} else{
-          sServo.eState = IDLE;
-				}
-        break;	
-    }
-}
-
-void DetectorInit(void){
-	IO0DIR &=~ DETECTOR_bm;
-}
-
-DetectorState eReadDetector(void){
-	if(IO0PIN & DETECTOR_bm){
-    return INACTIVE;
-	}else{
-	  return ACTIVE;
-  }
-}
 
 
 /**********************************************/
---
+++ Laby/Current/9_SERWO/servo.c
@@ -0,0 +1,65 @@
+#include <LPC21xx.H>
+
+#include "servo.h"
+#include "led.h"
+#include "timer_interrupts.h"
+
+
+struct Servo sServo;
+
+void ServoCallib(void){
+  sServo.eState=CALLIB;
+}
+
+void ServoInit(unsigned int uiServoFrequency){
+	ServoCallib();
+	LedInit();
+	Timer0Interrupts_Init(1000000/uiServoFrequency,&Automat);
+}
+
+void ServoGoTo(unsigned int uiPosition){
+	sServo.uiDesiredPosition=uiPosition;
+}
+
+void Automat(void){
+		
+	switch(sServo.eState){
+		  case CALLIB:
+			  if(ACTIVE == eReadDetector()){
+				  sServo.eState = IDLE;
+					sServo.uiCurrentPosition=0;
+					sServo.uiDesiredPosition=0;
+        }else{
+					LedStepLeft();
+				}
+			  break;
+      case IDLE:
+				if(sServo.uiCurrentPosition != sServo.uiDesiredPosition){
+          sServo.eState = IN_PROGRESS;
+				}
+        break;
+      case IN_PROGRESS:
+        if(sServo.uiCurrentPosition < sServo.uiDesiredPosition){
+          LedStepRight();
+					sServo.uiCurrentPosition++;
+				} else if(sServo.uiCurrentPosition > sServo.uiDesiredPosition){
+          LedStepLeft();
+					sServo.uiCurrentPosition--;
+				} else{
+          sServo.eState = IDLE;
+				}
+        break;	
+    }
+}
+
+void DetectorInit(void){
+	IO0DIR &=~ DETECTOR_bm;
+}
+
+DetectorState eReadDetector(void){
+	if(IO0PIN & DETECTOR_bm){
+    return INACTIVE;
+	}else{
+	  return ACTIVE;
+  }
+}
