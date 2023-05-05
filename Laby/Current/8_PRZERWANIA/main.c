0008eeb9cd77abad0156bb430a42bfd642e1ad2d
HalmonCelman <halmoncelman@gmail.com>
2023-05-05 20:48:25 +0200

    Przerwania 5

--- Laby/Current/8_PRZERWANIA/main.c
+++ Laby/Current/8_PRZERWANIA/main.c
@@ -1,15 +1,47 @@
 #include "led.h"
+#include "keyboard.h"
 #include "timer_interrupts.h"
 
+typedef enum {MOVE_LEFT, MOVE_RIGHT, STAY} LedState;
 
+
+void Automat(void);
+
+void Automat(void){
+	static LedState eLedState = STAY;
+	
+	switch(eLedState){
+      case MOVE_LEFT:
+        LedStepLeft();
+				if(BUTTON_1 == eKeyboardRead()){
+          eLedState = STAY;
+				}
+        break;
+      case STAY:
+        if(BUTTON_2 == eKeyboardRead()){
+				  eLedState=MOVE_RIGHT;
+        }
+				else if(BUTTON_0 == eKeyboardRead()){
+          eLedState = MOVE_LEFT;
+				}
+        break;
+			case MOVE_RIGHT:
+        LedStepRight();
+				if(BUTTON_1 == eKeyboardRead()){
+          eLedState = STAY;
+				}
+        break;	
+			
+    }
+}
 /**********************************************/
 int main (){
 	LedInit();
-	
-	unsigned int iMainLoopCtr;
-	Timer0Interrupts_Init(250000,&LedStepLeft);
+	KeyboardInit();
+	Timer0Interrupts_Init(20000,&Automat);
 
-	while(1){
-	 	iMainLoopCtr++;
+  while(1){
+  }
+	
+	return 0;
 	}
-}
