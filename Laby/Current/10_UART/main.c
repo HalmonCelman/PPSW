b2cd25156b9325c37fe6168fdc78524d97f8abc9
HalmonCelman <halmoncelman@gmail.com>
2023-05-07 02:11:43 +0200

    UART 3

--- Laby/Current/10_UART/main.c
+++ Laby/Current/10_UART/main.c
@@ -10,31 +10,48 @@
 
 /**********************************************/
 int main (){
-	LedInit();
+	
 	KeyboardInit();
 	DetectorInit();
 	
 	UART_InitWithInt(9600);
 	
+	ServoInit(50);
 	
 	
 
   while(1){
+		
 		switch(cOdebranyZnak){
-			case '1':
-				LedOn(0);
-			  break;
-			case '2':
-				LedOn(1);
-			  break;
-			case '3':
-				LedOn(2);
-			  break;
-			case '4':
-				LedOn(3);
-			  break;
 			case 'c':
-				LedOn(4);
+				ServoCallib();
+				break;
+			case '1':
+				ServoGoTo(50);
+				break;
+			case '2':
+				ServoGoTo(100);
+				break;
+			case '3':
+				ServoGoTo(150);
+				break;			
+			
+		}
+		
+		cOdebranyZnak=' ';
+		
+		switch(eKeyboardRead()){
+			case BUTTON_0:
+				ServoCallib();
+			  break;
+			case BUTTON_1:
+				ServoGoTo(12);
+			  break;
+			case BUTTON_2:
+				ServoGoTo(24);
+			  break;
+			case BUTTON_3:
+				ServoGoTo(36);
 			  break;
 		}
   }
