8142ea4edc6aaf9577ff1065db7a08bb559bb7ef
HalmonCelman <halmoncelman@gmail.com>
2023-05-23 21:48:45 +0200

    MODUŁY updated and datasheet added

--- Laby/Archive/5_MODULY/keyboard.c
+++ Laby/Archive/5_MODULY/keyboard.c
@@ -3,17 +3,18 @@
 #include "keyboard.h"
 
 KeyboardState eKeyboardRead(void){
-	if (!(IO0PIN & KEY0_bm)){
+	if (0 == (IO0PIN & KEY0_bm)){
 		return BUTTON_0;
-	}else if(!(IO0PIN & KEY1_bm)){
+	}else if(0 == (IO0PIN & KEY1_bm)){
 		return BUTTON_1;
-	}else if(!(IO0PIN & KEY2_bm)){
+	}else if(0 == (IO0PIN & KEY2_bm)){
 		return BUTTON_2;
-	}else if(!(IO0PIN & KEY3_bm)){
+	}else if(0 == (IO0PIN & KEY3_bm)){
 		return BUTTON_3;
 	}
 	return RELEASED;
 }
+
 
 void KeyboardInit(void){
 	IO0DIR &=~ (KEY0_bm | KEY1_bm | KEY2_bm | KEY3_bm);
