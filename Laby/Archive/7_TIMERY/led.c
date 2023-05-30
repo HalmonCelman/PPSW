a6323ff6fd05d6fb7cd0db1c9e6537a61a6e8345
HalmonCelman <halmoncelman@gmail.com>
2023-05-30 12:08:32 +0200

    Timery corrected - maski dodane

--- Laby/Archive/7_TIMERY/led.c
+++ Laby/Archive/7_TIMERY/led.c
@@ -1,14 +1,20 @@
 #include <LPC21xx.H>
 #include "led.h"
 
+#define LED0_bm (1<<16)
+#define LED1_bm (1<<17)
+#define LED2_bm (1<<18)
+#define LED3_bm (1<<19)
+
+typedef enum {LEFT,RIGHT} StepSide;
 
 void LedInit(void){
 	IO1DIR |= LED0_bm | LED1_bm | LED2_bm | LED3_bm;
-	IO1SET |= LED0_bm;
+	IO1SET = LED0_bm;
 }
 
 void LedOn(unsigned char ucLedIndeks){
-	IO1CLR |= LED0_bm | LED1_bm | LED2_bm | LED3_bm;
+	IO1CLR = LED0_bm | LED1_bm | LED2_bm | LED3_bm;
 	switch(ucLedIndeks){
 	case 0:
 		IO1SET = LED0_bm;
@@ -25,23 +31,14 @@ void LedOn(unsigned char ucLedIndeks){
 	}
 }
 
-static void LedStep(StepSide eStepSide){
+void LedStep(StepSide eStepSide){
 	static unsigned char ucCurrentDiode=0;
 	if(eStepSide == RIGHT ){
-		if(0 == ucCurrentDiode){
-			ucCurrentDiode=3;
-		}else{
 			ucCurrentDiode--;
-		}
-		
 	}else{
-		if(3 == ucCurrentDiode){
-			ucCurrentDiode=0;
-		}else{
 			ucCurrentDiode++;
-		}
 	}
-	LedOn(ucCurrentDiode);
+	LedOn(ucCurrentDiode%4);
 }
 
 void LedStepLeft(void){
--- Laby/Archive/7_TIMERY/timer.c
+++ Laby/Archive/7_TIMERY/timer.c
@@ -1,27 +1,33 @@
 #include <LPC21xx.H>
 #include "timer.h"
 
+#define mTIMER0_ENABLE (1<<0)
+#define mTIMER0_RESET (1<<1)
+#define mTIMER0_MR0_INTERRUPT_ENABLE (1<<0)
+#define mTIMER0_MR0_RESET_TC (1<<1)
+#define mTIMER0_MR0_INTERRUPT_FLAG (1<<0)
+
 void InitTimer0(void){
-  T0TCR |= (1<<0);
+  T0TCR = mTIMER0_ENABLE;
 }
 
 
 void WaitOnTimer0(unsigned int uiTime){
-	T0TCR |= (1<<1);
-	T0TCR &=~ (1<<1);
+	T0TCR |= mTIMER0_RESET;
+	T0TCR &=~ mTIMER0_RESET;
   while( T0TC<(uiTime*15) );
 }
 
 void InitTimer0Match0(unsigned int iDelayTime){
 	T0MR0 = iDelayTime*15;
-	T0MCR |= (1<<0) | (1<<1);
-	T0TCR |= (1<<1);
-	T0TCR &=~ (1<<1);
+	T0MCR |= mTIMER0_MR0_INTERRUPT_ENABLE | mTIMER0_MR0_RESET_TC;
+	T0TCR |= mTIMER0_RESET;
+	T0TCR &=~ mTIMER0_RESET;
 	InitTimer0();
 }
 
 void WaitOnTimer0Match0(void){
-	while(!(T0IR & (1<<0)));
-	T0IR |= (1<<0);
+	while(0 == (T0IR & mTIMER0_MR0_INTERRUPT_FLAG));
+	T0IR = mTIMER0_MR0_INTERRUPT_FLAG;
 }
 
